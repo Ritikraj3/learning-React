107a9cd5e0afa4f6f97745c9ec27599b65009b9d
Ritikraj3 <ritikraj5776@gmail.com>
2025-03-21 18:16:35 +0530

    source- maps

---
+++ 05_Source-map/script.js
@@ -0,0 +1,37 @@
+const container = 
+<div className ="container" id="container">
+        <section>
+            <p>I am learning React.js</p>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/3/30/React_Logo_SVG.svg" 
+            style={{ 
+                width: 200,
+                backgroundColor: 'black' , 
+                borderRadius:12, 
+                padding: 32}} />
+        </section>
+        <section>
+            <form> 
+                <div className ="input-container">
+                    <label htmlFor="username" 
+                    style={{cursor: 'pointer' }}>username</label>
+                    <input id="username" />
+                </div>
+
+                <div className ="input-container">
+                    <label htmlFor="password" 
+                    style={{cursor: 'pointer' }}>username</label>
+                    <input id="password" type="password" />
+                </div>
+            </form>
+        </section>
+    </div>
+
+console.log(container);
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(container)
+
+
+
+
