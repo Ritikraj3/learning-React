a1fb24d6e830f5cc64bb4d7986f6730c9553a6b5
Ritikraj3 <ritikraj5776@gmail.com>
2025-04-05 18:13:32 +0530

    useState working

---
+++ 12_State-vs-Props/components/Counter.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState('Ritik raj');
+  return (
+    <div>
+      <div style={{ textAlign: "center" }}>
+        <h1>{count}</h1>
+        <h1>{name}</h1>
+        <button
+          style={{ cursor: "pointer" }}
+          onClick={() => {
+            setName(name);
+            setCount((previousState) => previousState + 1);
+            setCount((previousState) => previousState + 1);
+            setCount((previousState) => previousState + 1);
+          }}
+        >
+          Increase Count
+        </button>
+
+        <button
+          style={{ cursor: "pointer", marginLeft: "10px" }}
+          onClick={() => {
+            setCount(0);
+          }}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
