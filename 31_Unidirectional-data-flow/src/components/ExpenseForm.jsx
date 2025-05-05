90415c0adc9c0ba43be1eab9bd3680cefbf9c0af
Ritikraj3 <ritikraj5776@gmail.com>
2025-05-05 12:59:05 +0530

    One-way-data-binding

---
+++ 31_Unidirectional-data-flow/src/components/ExpenseForm.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+export default function ExpenseForm({ setExpenses }) {
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const expense = {title, category, amount, id:crypto.randomUUID()}
+    setExpenses((prevState) => [...prevState, expense])
+    setTitle('')
+    setCategory('')
+    setAmount('')
+  };
+
+  return (
+    <form className="expense-form" onSubmit={handleSubmit}>
+      <div className="input-container">
+        <label htmlFor="title">Title</label>
+        <input
+          id="title"
+          name="title"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+      </div>
+      <div className="input-container">
+        <label htmlFor="category">Category</label>
+        <select
+          id="category"
+          name="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="" hidden>
+            Select Category
+          </option>
+          <option value="grocery">Grocery</option>
+          <option value="clothes">Clothes</option>
+          <option value="bills">Bills</option>
+          <option value="education">Education</option>
+          <option value="medicine">Medicine</option>
+        </select>
+      </div>
+      <div className="input-container">
+        <label htmlFor="amount">Amount</label>
+        <input
+          id="amount"
+          name="amount"
+          value={amount}
+          onChange={(e) => {
+            setAmount(e.target.value);
+          }}
+        />
+      </div>
+      <button className="add-btn">Add</button>
+    </form>
+  );
+}
