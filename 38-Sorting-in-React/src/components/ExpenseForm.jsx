518203c024cbbee5a9c82d1940ecdf321d7dbabb
Ritikraj3 <ritikraj5776@gmail.com>
2025-05-29 00:12:38 +0530

    sorting

---
+++ 38-Sorting-in-React/src/components/ExpenseForm.jsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from "react";
+import { useRef } from "react";
+import Input from "./Input";
+import SelectMenu from "./SelectMenu";
+
+export default function ExpenseForm({
+  setExpenses,
+  expense,
+  setExpense,
+  editingRowId,
+  setEditingRowId
+}) {
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setExpense((prev) => ({ ...prev, [name]: value }));
+    setErrors({});
+  };
+
+  const validationConfig = {
+    title: [
+      { required: true, message: "Please enter title" },
+      {
+        minLength: 5,
+        message: "Title should be at least 5 character long",
+      },
+    ],
+    category: [{ required: true, message: "Please select a category" }],
+    amount: [{ required: true, message: "Please enter an amount" },
+      { pattern: /^-?(0|[1-9]\d*)$/ , message: "Please enter a valid number" }
+    ],
+  };
+
+  const validate = (formData) => {
+    const errorsData = {};
+
+    Object.entries(formData).forEach(([key, value]) => {
+      validationConfig[key].some((rule) => {
+        if (rule.required && !value) {
+          errorsData[key] = rule.message;
+          return true;
+        }
+
+        if (rule.minLength && value.length < 5) {
+          errorsData[key] = rule.message;
+          return true;
+        }
+
+        if (rule.pattern && !rule.pattern.test(value)) {
+          errorsData[key] = rule.message
+          return true
+        }
+      });
+    });
+
+    setErrors(errorsData);
+    return errorsData;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const validateResult = validate(expense);
+    if (Object.keys(validateResult).length) return;
+
+    if (editingRowId) {
+      setExpenses((preState) => 
+        preState.map((prevExpense) => {
+          if (prevExpense.id === editingRowId) {
+            return {...expense, id: editingRowId}
+          }
+          return prevExpense
+        })
+      )
+      setEditingRowId('')
+      return
+    }
+
+    setExpenses((prevState) => [
+      ...prevState,
+      { ...expense, id: crypto.randomUUID() },
+    ]);
+
+    setExpense({
+      title: "",
+      category: "",
+      amount: "",
+    });
+  };
+
+  return (
+    <form className="expense-form" onSubmit={handleSubmit}>
+      <Input
+        Label="Title"
+        id="title"
+        name="title"
+        value={expense.title}
+        onChange={handleChange}
+        error={errors.title}
+      />
+
+      <SelectMenu
+        Label="Category"
+        name="category"
+        value={expense.category}
+        onChange={handleChange}
+        options={["Grocery", "Clothes", "Bills", "Education", "Medicine"]}
+        defaultOption="Select Category"
+        error={errors.category}
+      />
+
+      <Input
+        Label="Amount"
+        id="amount"
+        name="amount"
+        value={expense.amount}
+        onChange={handleChange}
+        error={errors.amount}
+      />
+      <button className="add-btn"> {editingRowId ? "Save" : "Add"} </button>
+    </form>
+  );
+}
