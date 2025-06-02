6c21bd6aa83bc6ad7c23c09529c23bbf874c0488
Ritikraj3 <ritikraj5776@gmail.com>
2025-06-03 01:23:46 +0530

    Life-Cycle in Class-based-component

---
+++ 45_LifeCycle-method-in-react/src/components/CounterOld.jsx
@@ -0,0 +1,64 @@
+import { Component } from "react";
+
+class CounterOld extends Component {
+  constructor() {
+    super()
+    this.state = {
+        count: 0, 
+    }
+    this.timerId = null
+    console.log('Constructor')
+  }
+
+  componentDidMount() {
+    console.log('ComponentDidMount')
+
+    // this.timerId = setInterval(() => {
+    //   console.log("Hii Old Counter")
+    // }, 1000)
+  }
+
+  componentDidUpdate(){
+    console.log('ComponentDidUpdate')
+  }
+
+  componentWillUnmount(){
+    console.log('ComponentWillUnmount')
+
+    clearInterval(this.timerId)
+  }
+
+
+  render() {
+    console.log('Rendering')
+
+    const { name } = this.props;
+    const { count } = this.state    
+    return (
+      <>
+        <h1 className="text-3xl font-bold text-center  text-blue-600 my-4">
+          {name}
+        </h1>
+        <div className="flex items-center justify-between max-w-md mx-auto mt-4 px-4">
+          <button
+            onClick={() => this.setState({ count: count + 1})}
+            className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded"
+          >
+            Increment
+          </button>
+
+          <h1 className="text-2xl px-2 font-semibold"> Count: {count} </h1>
+
+          <button
+            onClick={() => this.setState({ count: count - 1})}
+            className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded"
+          >
+            Decrement
+          </button>
+        </div>
+      </>
+    );
+  }
+}
+
+export default CounterOld;
