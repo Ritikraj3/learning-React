e29bbe4f17460386e195a7692fb112f440f6c9dc
Ritikraj3 <ritikraj5776@gmail.com>
2025-06-03 16:41:23 +0530

    Higher-Order component

---
+++ 47_Higher-Order-Component/src/components/ClickCounter.jsx
@@ -0,0 +1,29 @@
+import { Component } from "react";
+import withCounter from "./WithCounter";
+
+class ClickCounter extends Component {
+  
+  render() {
+    const { name } = this.props;
+    return (
+      <>
+        <h1 className="text-3xl font-bold text-center  text-blue-600 my-4">
+          {name}
+        </h1>
+        <div className="flex items-center  justify-between max-w-md mx-auto mt-4 px-4">
+
+          {/* <h1 onClick = {() => this.setState({ count: count + 1 })} className="text-2xl select-none font-semibold cursor-pointer bg-green-500 hover:bg-green-600 py-1 px-4 text-white rounded"> Count: {count} </h1> */}
+
+          <button
+            onClick={this.props.increaseCount}
+            className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded"
+          >
+            Count: {this.props.count}
+          </button>
+        </div>
+      </>
+    );
+  }
+}
+
+export default withCounter(ClickCounter);
---
+++ 47_Higher-Order-Component/src/components/WithCounter.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+const withCounter = (WrappedComponent) => {
+  return class NewComponent extends Component {
+    constructor() {
+      super();
+      this.state = {
+        count: 0,
+      };
+    }
+
+    increaseCount = () => {
+      this.setState({ count: this.state.count + 1 });
+    };
+    render() {
+      return (
+        <WrappedComponent
+          count={this.state.count}
+          increaseCount={this.increaseCount}
+          {...this.props}
+        />
+      );
+    }
+  };
+};
+
+export default withCounter;
